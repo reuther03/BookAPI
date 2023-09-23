fe4d6009c2a6b516897da9729f89ffc2d93f4756
Reuther <michaldrozd03@gmail.com>
2023-09-24 01:13:17 +0200

    + Added 2nd endpoint and dtos

--- BookAPI/Controllers/CountriesController.cs
+++ BookAPI/Controllers/CountriesController.cs
@@ -2,7 +2,7 @@
 
 namespace BookAPI;
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class CountriesController : ControllerBase
 {
     private readonly ICountryRepository _countryRepository;
@@ -11,12 +11,49 @@ public class CountriesController : ControllerBase
     {
         _countryRepository = countryRepository;
     }
+
     [HttpGet]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<CountryDto>))]
     public IActionResult GetAllCountries()
     {
         var result = _countryRepository.GetCountries();
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
 
-        return Ok(result);
+        var countriesDto = new List<CountryDto>();
+        foreach (var country in result)
+        {
+            countriesDto.Add(new CountryDto
+            {
+                Id = country.Id,
+                Name = country.Name
+            });
+        }
+        return Ok(countriesDto);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(200, Type = typeof(CountryDto))]
+    public IActionResult GetCountry(int id)
+    {
+        if (!_countryRepository.CountryExists(id))
+            return NotFound();
+
+        var result = _countryRepository.GetCountry(id);
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var countryDto = new CountryDto()
+        {
+            Id = result.Id,
+            Name = result.Name
+        };
+
+        return Ok(countryDto);
     }
 }
 
