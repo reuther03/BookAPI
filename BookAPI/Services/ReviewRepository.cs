911b3a268ae6ce8c94e04e6ee4b8043b47af8c21
Reuther <michaldrozd03@gmail.com>
2023-09-27 01:20:01 +0200

    + Added ReviewRepository

--- BookAPI/Services/ReviewRepository.cs
+++ BookAPI/Services/ReviewRepository.cs
@@ -8,17 +8,45 @@ public class ReviewRepository : IReviewRepository
     private readonly DbSet<Review> _reviews;
     public ICollection<Review> GetBookReviews(int bookId)
     {
-        throw new NotImplementedException();
+        return _reviews
+            .Where(r => r.Book.Id == bookId)
+            .ToList();
     }
 
     public Review GetReview(int reviewId)
     {
-        throw new NotImplementedException();
+        return _reviews
+            .FirstOrDefault(r => r.Id == reviewId);
     }
 
     public Book GetReviewBook(int reviewId)
     {
-        throw new NotImplementedException();
+        return _reviews
+            .Include(r => r.Book)
+            .FirstOrDefault(r => r.Id == reviewId)  // Review
+            .Book;                                  // Review.Book
+
+        /*
+        DB
+        {
+            "id": 1,
+            "headline": "bla bla",
+            ...
+            "bookId": 1 // <- DB nie moze miec zagniezdzonego obiektu
+        }
+
+        .NET
+        {
+            "id": 1,
+            "headline": "bla bla",
+            ...
+            "book": {
+                "id": 1,
+                "title": "bla title",
+                "datePublished": "1903-01-01T00:00:00Z
+            }
+        }
+        */
     }
 
     public ICollection<Review> GetReviews()
